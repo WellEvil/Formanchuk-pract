e3a141725a0d8f3c44ae6c69e3c0ec765c3e2b4d
WellEvil <wellevil028@gmail.com>
2024-02-03 18:01:31 +0700

    123

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import MainPage from './Pages/main';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function App() {
-  return <div className="App">
-    <BrowserRouter>
-      <Routes>
-        <Route path={'/'} element={''} />
-        <Route path={'/main'} element={<MainPage />} />
-      </Routes>
-    </BrowserRouter>
-  </div>
-}
+
+  function App() {
+    return <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path={'/'} element={''} />
+          <Route path={'/main'} element={<MainPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  }
 
 export default App;
