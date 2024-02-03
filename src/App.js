54eab1816c4e74878cb2c9624066257a5dd6ba20
WellEvil <138964098+WellEvil@users.noreply.github.com>
2024-02-03 17:00:05 +0700

    создание главной стр

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom'
+import MainPage from './pages/main'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vlad kryt
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/'} element={''} />
+        <Route path={'/main'} element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
+  </div>
 }
 
 export default App;
